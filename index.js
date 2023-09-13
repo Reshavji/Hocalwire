5491ba6beea65c3dfa4500d01764ae6b2f5df701
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-09-13 10:27:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-let n = null;
-const a =0;
-a =a+10;
-    console.log(a);
+// Get references to the main content and window
+const mainContent = document.querySelector('main');
+const windowHeight = window.innerHeight;
 
+// Add an event listener to detect browser scroll
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY;
+
+  // Set the main content's scroll position to match the browser scroll position
+  mainContent.scrollTop = scrollPosition;
+});
+
+// Update the main content's height to match the window height
+mainContent.style.height = `${windowHeight}px`;
